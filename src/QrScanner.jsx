f6b653d5262e9cbafbe0cceb32135c6da12b7ce0
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2022-06-28 22:36:42 +0530

    firebase hosting initialized

--- src/QrScanner.jsx
+++ src/QrScanner.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import Header from "./Header";
-// import { QrReader } from "react-qr-reader";
+import { QrReader } from "react-qr-reader";
+// import { useNavigate} from "react-router-dom";
 function QrScanner() {
   // const [data, setData] = useState("No result");
+// const navigate = useNavigate();
+var visible= true;
+  function handleClick(res){
+    visible=false;
+    console.log(visible);
+    // navigate("/scannerDetails")
+  }
   return (
     <div>
       <Header />
-      {/* <QrReader
+     { visible ? <QrReader
         onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
-
-          if (!!error) {
+          if (result) {
+            console.log(result.text)
+            handleClick(result.text);
+          }else if (error) {
             console.info(error);
+            // setData("error")
           }
         }}
         style={{ width: "100%" }}
-      /> */}
-      <p style={{ marginTop: "200px" }}>QRScanner</p>
+      /> : <div>{{visible}}</div>
+     }
+      <p onClick={{handleClick}} style={{ marginTop: "200px" }}>QRScanner</p>
+      <p></p>
     </div>
   );
 }
