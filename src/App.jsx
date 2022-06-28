9a59a151f81f2e66d1ac15430f0fec7be232fe6f
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2022-06-28 15:17:00 +0530

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import Header from "./Header.jsx";
+// import QrScan from "./qrScan";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h3>hello</h3>
+      {/* <QrScan /> */}
+    </div>
+  );
+}
+
+export default App;
